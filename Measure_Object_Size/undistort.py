38b1180e7eb1176dfd90c21e0752a6cea4b206fc
Ario-Lab18 <142185112+Ario-Lab18@users.noreply.github.com>
2025-02-10 17:33:55 -0800

    Fixed undistort coefficent

--- Measure_Object_Size/undistort.py
+++ Measure_Object_Size/undistort.py
@@ -2,14 +2,14 @@ import cv2
 import numpy as np
 
 
-img_path = '/Users/krishnanujam/Desktop/captured_image.jpeg'
+img_path = '/Users/aarav/MATE/Test2.jpeg'
 distorted_img = cv2.imread(img_path)
 if distorted_img is None:
     raise FileNotFoundError(f"Image not found at {img_path}")
 
 
-K = np.array([[3000, 0, distorted_img.shape[1] / 2],
-              [0, 3000, distorted_img.shape[0] / 2],
+K = np.array([[1700, 0, distorted_img.shape[1] / 2],
+              [0, 1700, distorted_img.shape[0] / 2],
               [0, 0, 1]], dtype=np.float32)
 D = np.array([-0.4, 0.1, 0, 0], dtype=np.float32)
 
@@ -20,6 +20,6 @@ map1, map2 = cv2.fisheye.initUndistortRectifyMap(K, D, np.eye(3), new_K, (w, h),
 undistorted_img = cv2.remap(distorted_img, map1, map2, interpolation=cv2.INTER_LINEAR, borderMode=cv2.BORDER_CONSTANT)
 
 
-output_path = '/Users/krishnanujam/Desktop/undistorted.jpg'
+output_path = '/Users/aarav/MATE/Test1600.jpeg'
 cv2.imwrite(output_path, undistorted_img)
 print(f"Undistorted image sabre {output_path}")
